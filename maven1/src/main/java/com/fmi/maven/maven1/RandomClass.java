e721bec91c40327693b09976e1095bf4fc5a198b
test <test@test.com>
2017-04-09 10:35:02 +0300

    create new project

---
+++ maven1/src/main/java/com/fmi/maven/maven1/RandomClass.java
@@ -0,0 +1,14 @@
+package com.fmi.maven.maven1;
+
+public class RandomClass {
+
+	 public void print (final String prt )
+	    {
+		 if("Ivan".equals(prt)){
+			 System.out.println(prt);
+		 }else{
+			 System.out.println("wrong name"); 
+		 }
+	        
+	    }
+}
---
+++ maven1/src/test/java/com/fmi/maven/maven1/RandomClassTest.java
@@ -0,0 +1,14 @@
+package com.fmi.maven.maven1;
+
+import org.junit.Test;
+
+public class RandomClassTest {
+	@Test
+	public void testPrint(){
+		
+		final RandomClass instance = new RandomClass();
+		instance.print(null);
+		
+	}
+
+}
