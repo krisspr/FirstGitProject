944fb19ad4936e3fa3456f2808723e47a34bfc4a
test <test@test.com>
2017-04-09 10:51:30 +0300

    gfgfg

--- maven1/src/main/java/com/fmi/maven/maven1/RandomClass.java
+++ maven1/src/main/java/com/fmi/maven/maven1/RandomClass.java
@@ -4,7 +4,7 @@ public class RandomClass {
 
 	 public void print (final String prt )
 	    {
-		 if("Ivan".equals(prt)){
+		 if("Vasil".equals(prt)){
 			 System.out.println(prt);
 		 }else{
 			 System.out.println("wrong name"); 
